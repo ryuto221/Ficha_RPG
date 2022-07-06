efbb9bb0c95ccf51a91a4686350de65f6b3a6028
Luiz Felipe <luizfelipe01700@gmail.com>
2022-07-06 14:02:43 -0300

    fiz o front do cadastro

--- paginas/php/cadastro.php
+++ paginas/php/cadastro.php
@@ -1,38 +1,46 @@
 <?php
-    session_start();
     require("../template/header.php");
     $nomeEr = "";
     $emailEr = "";
     $confEmailEr = "";
     $senhaEr = "";
     $confSenhaEr = "";
+    $nome = "";
+    $email = "";
+    $confEmail = "";
+    $senha = "";
+    $confSenha = "";
+    $mestre = "";
 ?>
 <body>
     <div class="corpo">
         <div class="caContainer">
             <form action="" method="POST">
-                <h1>Cadastro</h1>
-                <p>Se já é cadastrado faça <a href="login.php">Login</a></p>
+                <h1>Registration</h1>
+                <p>Has an acount make it <a href="login.php">Login</a></p>
                 <br>
-                <label class="sobTxt">Nome</label><br>
+                <label class="sobTxt">Name</label><br>
                 <input type="text" name="nome" class="txt"><br>
                 <span class="spanErro"><?php echo $nomeEr; ?></span><br>
                 <label class="sobTxt">E-mail</label><br>
                 <input type="email" name="email" class="txt"><br>
                 <span class="spanErro"><?php echo $emailEr; ?></span><br>
-                <label class="sobTxt">Confirmar o e-mail</label><br>
+                <label class="sobTxt">Confirm o e-mail</label><br>
                 <input type="email" name="conFemail" class="txt"><br>
                 <span class="spanErro"><?php echo $confEmailEr; ?></span><br>
-                <label class="sobTxt">Senha</label><br>
+                <label class="sobTxt">Password</label><br>
                 <input type="password" name="senha" class="txt"><br>
                 <span class="spanErro"><?php echo $senhaEr; ?></span><br>
-                <label class="sobTxt">Confirmar senha</label><br>
+                <label class="sobTxt">Confirm password</label><br>
                 <input type="password" name="confSenha" class="txt"><br>
                 <span class="spanErro"><?php echo $confSenhaEr; ?></span><br>
+                <label>I'm a mester of a rpg <input type="checkbox" name="mestre"></label>
+                <br>
+                <br>
                 <div class="btnDiv">
-                    <input type="submit" name="enviar" value="Enviar" class="btn">
+                    <input type="submit" name="enviar" value="Confirm" class="btn">
                     |||
-                    <input type="reset" value="Limpar" class="btn">
+                    <input type="reset" value="Clear" class="btn">
                 </div>
             </form>
         </div>
